b2c6155708624e69b9b0c934b2154b72e7e1dd26
Radsonb <radsonbrendon02@gmail.com>
2024-08-07 08:31:46 -0300

    Payment, on-to-one association

--- jpa/src/main/java/course/example/jpa/config/TestConfig.java
+++ jpa/src/main/java/course/example/jpa/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import course.example.jpa.entities.Category;
 import course.example.jpa.entities.Order;
 import course.example.jpa.entities.OrderItem;
+import course.example.jpa.entities.Payment;
 import course.example.jpa.entities.Product;
 import course.example.jpa.entities.User;
 import course.example.jpa.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 
 	}
 	
